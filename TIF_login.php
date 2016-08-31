cd8964eb06a082390be76005ab17c9b518c5216d
Kim Lofthouse <kimmiikal@gmail.com>
2016-08-31 11:16:09 +0100

    Add files via upload
    TIF login form

---
+++ TIF_login.php
@@ -0,0 +1,8 @@
+<form method ="post" action="">
+  Username:<input name="username" type="text">
+  Password:<input name ="password" type="password">
+  <button type="submit">Log in</button>
+</form>
+
+<?php
+login(); ?>
